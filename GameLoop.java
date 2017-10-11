14fdba464206c1e19544ac26c78d398f8189f880
lillianiwaniszyn <lillian.iwaniszyn@gmail.com>
2017-10-11 01:03:05 -0600

    moved everything from main into gameloop class

--- GameLoop.java
+++ GameLoop.java
@@ -1,5 +1,7 @@
 package Tests;
 
+import java.util.Scanner;
+
 public class GameLoop {
     public void printMainScreen() {
         System.out.println("Choose your HERO");
@@ -46,5 +48,92 @@ public class GameLoop {
         System.out.println("Fight again?");
         System.out.println("1 for Yes");
         System.out.println("2 for No");
+    }
+    public void pickBattleLoop(){
+        int hero, match, restart;
+        Battle bat = new Battle();
+        Player pikachu = new Player(6,0,19);
+        Player celebi = new Player(8,1,16);
+        Player snorlax = new Player(5,2,18);
+        Player charizard = new Player(10,0,15);
+        Player ditto = new Player(7,1,17);
+        Player eevee = new Player(13,2,10);
+        Player pokemon = new Player(1,1,1);
+        Scanner input = new Scanner(System.in);
+        hero = input.nextInt();
+        System.out.println("You have chosen " + pokemon.getPlayer(hero));
+
+            while(hero == 1){
+
+                System.out.println("Your battle is against....");
+                System.out.println("");
+                match = bat.getRandomWithExclusion(1,6,1);
+                bat.printBattle(match, pikachu);
+                printFightAgain();
+                restart = input.nextInt();
+                if(playAgain(restart) == true)
+                	break;
+
+                    }
+                    
+            while(hero == 2){
+                System.out.println("Your battle is against....");
+                System.out.println("");
+                match = bat.getRandomWithExclusion(1,6,2);
+                bat.printBattle(match, celebi);
+                printFightAgain();
+                restart = input.nextInt();
+                if(playAgain(restart) == true)
+                	break;
+
+                    }
+                    
+            while(hero == 3){
+                System.out.println("Your battle is against....");
+                System.out.println("");
+                match = bat.getRandomWithExclusion(1,6,3);
+                bat.printBattle(match, snorlax);
+                printFightAgain();
+                restart = input.nextInt();
+                if(playAgain(restart) == true)
+                	break;
+
+                    }
+                    
+            while(hero == 4){
+                System.out.println("Your battle is against....");
+                System.out.println("");
+                match = bat.getRandomWithExclusion(1,6,4);
+                bat.printBattle(match, charizard);
+                printFightAgain();
+                restart = input.nextInt();
+                if(playAgain(restart) == true)
+                	break;
+
+                    }
+                    
+            while(hero == 5){
+                System.out.println("Your battle is against....");
+                System.out.println("");
+                match = bat.getRandomWithExclusion(1,6,5);
+                printFightAgain();
+                restart = input.nextInt();
+                if(playAgain(restart) == true)
+                	break;
+
+                    }
+                    
+            while(hero == 6){
+                System.out.println("Your battle is against....");
+                System.out.println("");
+                match = bat.getRandomWithExclusion(1,6,6);
+                bat.printBattle(match, eevee);
+                printFightAgain();
+                restart = input.nextInt();
+                if(playAgain(restart) == true)
+                	break;
+
+                    }
+    	
     }
 }
--- Main.java
+++ Main.java
@@ -7,95 +7,15 @@ public class Main {
 public static void main(String[] args) {
 	
     @SuppressWarnings("resource")
-    Scanner input = new Scanner(System.in);
+    
 
-    int hero, match, restart;
-    Battle bat = new Battle();
-    Player pikachu = new Player(6,0,19);
-    Player celebi = new Player(8,1,16);
-    Player snorlax = new Player(5,2,18);
-    Player charizard = new Player(10,0,15);
-    Player ditto = new Player(7,1,17);
-    Player eevee = new Player(13,2,10);
+    //
+
     GameLoop game = new GameLoop();
-    Player pokemon = new Player(1,1,1);
-
-
+    
     game.printMainScreen();
-    hero = input.nextInt();
-    System.out.println("You have chosen " + pokemon.getPlayer(hero));
 
-        while(hero == 1){
 
-            System.out.println("Your battle is against....");
-            System.out.println("");
-            match = bat.getRandomWithExclusion(1,6,1);
-            bat.printBattle(match, pikachu);
-            game.printFightAgain();
-            restart = input.nextInt();
-            if(game.playAgain(restart) == true)
-            	break;
-
-                }
-                
-        while(hero == 2){
-            System.out.println("Your battle is against....");
-            System.out.println("");
-            match = bat.getRandomWithExclusion(1,6,2);
-            bat.printBattle(match, celebi);
-            game.printFightAgain();
-            restart = input.nextInt();
-            if(game.playAgain(restart) == true)
-            	break;
-
-                }
-                
-        while(hero == 3){
-            System.out.println("Your battle is against....");
-            System.out.println("");
-            match = bat.getRandomWithExclusion(1,6,3);
-            bat.printBattle(match, snorlax);
-            game.printFightAgain();
-            restart = input.nextInt();
-            if(game.playAgain(restart) == true)
-            	break;
-
-                }
-                
-        while(hero == 4){
-            System.out.println("Your battle is against....");
-            System.out.println("");
-            match = bat.getRandomWithExclusion(1,6,4);
-            bat.printBattle(match, charizard);
-            game.printFightAgain();
-            restart = input.nextInt();
-            if(game.playAgain(restart) == true)
-            	break;
-
-                }
-                
-        while(hero == 5){
-            System.out.println("Your battle is against....");
-            System.out.println("");
-            match = bat.getRandomWithExclusion(1,6,5);
-            game.printFightAgain();
-            restart = input.nextInt();
-            if(game.playAgain(restart) == true)
-            	break;
-
-                }
-                
-        while(hero == 6){
-            System.out.println("Your battle is against....");
-            System.out.println("");
-            match = bat.getRandomWithExclusion(1,6,6);
-            bat.printBattle(match, eevee);
-            game.printFightAgain();
-            restart = input.nextInt();
-            if(game.playAgain(restart) == true)
-            	break;
-
-                }
                 }
                 }
                 
